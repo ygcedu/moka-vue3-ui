8d66fdcc39a3e1e69590a00c851c26838fe83e58
ygcedu <937601471@qq.com>
2021-08-31 22:39:04 +0800

    动画流畅了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -36,6 +36,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 
 button.checked {
@@ -44,5 +45,9 @@ button.checked {
 
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
 }
 </style>

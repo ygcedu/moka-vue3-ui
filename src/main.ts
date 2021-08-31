5d0aad92b42791944d8ddc6d50e0a8e370599bf9
ygcedu <937601471@qq.com>
2021-08-31 22:01:22 +0800

    路由关闭之后切换 aside（手机上）

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
 import App from './App.vue';
 import './index.scss';
-import {createRouter, createWebHashHistory} from 'vue-router';
 import {createApp} from 'vue';
-import Home from './views/Home.vue';
-import Doc from './views/Doc.vue';
-
-const history = createWebHashHistory();
-const router = createRouter({
-  history: history,
-  routes: [
-    {path: '/', component: Home},
-    {path: '/doc', component: Doc}
-  ]
-});
+import {router} from './router';
 
 const app = createApp(App);
 app.use(router);
---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import {createRouter, createWebHashHistory} from 'vue-router';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
+import SwitchDemo from './components/SwitchDemo.vue';
+
+const history = createWebHashHistory();
+export const router = createRouter({
+  history: history,
+  routes: [
+    {path: '/', component: Home},
+    {
+      path: '/doc', component: Doc, children: [
+        {path: 'switch', component: SwitchDemo}
+      ]
+    }
+  ]
+});

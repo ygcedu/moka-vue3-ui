82e32b99302e5bc440de81b2fc2a460f16319ed6
ygcedu <937601471@qq.com>
2021-08-31 22:25:12 +0800

    Switch css 1

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <p>Switch 示例</p>
+    <Switch/>
   </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+
+export default {
+  components: {Switch}
+};
+</script>

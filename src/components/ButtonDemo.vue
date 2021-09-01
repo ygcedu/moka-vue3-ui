3b9b3d4461b07cd4152e2aee9dec74500a6e4bc9
ygcedu <937601471@qq.com>
2021-09-01 22:51:31 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <p>Button 示例</p>
+    Button 示例
+    <h1>示例1</h1>
+    <div>
+      <Button @click="onClick">你好</Button>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+
+export default {
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('hi');
+    };
+    return {onClick};
+  }
+};
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :size="size">
+    <!--<button v-bind="$attrs">-->
+    <button v-bind="rest">
+      <slot/>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  // 取消默认绑定（根元素）
+  inheritAttrs: false,
+  props: {},
+  setup(props, context) {
+    const {size, ...rest} = context.attrs;
+    return {size, rest};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>

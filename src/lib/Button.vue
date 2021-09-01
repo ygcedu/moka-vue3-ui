e259fc7b04b80b11cc2bf8e515137fe322a46831
ygcedu <937601471@qq.com>
2021-09-02 00:38:32 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,13 +16,18 @@ export default {
       type: String,
       default: 'normal',
     },
+    level: {
+      type: String,
+      default: 'normal',
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`arw-theme-${theme}`]: theme,
         [`arw-size-${size}`]: size,
+        [`arw-level-${level}`]: level,
       };
     });
     return {classes};
@@ -35,6 +40,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .arw-button {
   box-sizing: border-box;
   height: $h;
@@ -49,14 +55,18 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
+
   & + & {
     margin-left: 8px;
   }
+
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
   }
+
   &:focus {
     outline: none;
   }
@@ -72,12 +82,10 @@ $radius: 4px;
       color: lighten($blue, 10%);
     }
   }
-
   &.arw-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-
     &:hover,
     &:focus {
       background: darken(white, 5%);
@@ -94,6 +102,63 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+
+  &.arw-theme-button {
+    &.arw-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.arw-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.arw-theme-link {
+    &.arw-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.arw-theme-text {
+    &.arw-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.arw-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>

93a0d064fcb718c63273de742bc625b4d0f0c442
ygcedu <937601471@qq.com>
2021-09-02 23:17:14 +0800

    openDialog 一句话打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,26 +1,25 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <div style="position: relative; z-index: 1;">
-    <Button @click="toggle">toggle</Button>
-    <!--<Dialog :visible="x" @update:visible="x=$event"></Dialog>-->
-    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
-      <template v-slot:content>
-        <strong>你好</strong>
-        <div>hi</div>
-      </template>
-      <template v-slot:title>
-        <strong>加粗的标题</strong>
-      </template>
-    </Dialog>
-  </div>
-  <div style="position: relative; z-index: 2; width: 300px; height: 300px; background: red;"></div>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+    <template v-slot:content>
+      <strong>你好</strong>
+      <div>hi</div>
+    </template>
+    <template v-slot:title>
+      <strong>加粗的标题</strong>
+    </template>
+  </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
 import {ref} from 'vue';
+import {openDialog} from '../lib/openDialog';
 
 export default {
   components: {
@@ -36,11 +35,25 @@ export default {
     };
     const f2 = () => {
     };
+    const showDialog = () => {
+      openDialog({
+        title: '标题',
+        content: '你好',
+        ok() {
+          console.log('ok');
+        },
+        cancel() {
+          console.log('cancel');
+        },
+        closeOnClickOverlay: false
+      });
+    };
     return {
       x,
       toggle,
       f1,
-      f2
+      f2,
+      showDialog
     };
   }
 };
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -60,7 +60,7 @@ export default {
       }
     };
     const cancel = () => {
-      context.emit('cancel');
+      props.cancel?.();
       close();
     };
 

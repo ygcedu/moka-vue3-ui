8b0ecd6030b8685135ccdfbbcd8feaf105975a52
ygcedu <937601471@qq.com>
2021-09-02 21:30:52 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <p>Dialog 示例</p>
-  </div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+
+export default {
+  components: {
+    Dialog
+  }
+};
+</script>

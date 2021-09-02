a7717ff9d27f3833c03a49b7870cddfec12313a0
ygcedu <937601471@qq.com>
2021-09-02 22:27:36 +0800

    Dialog 支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,38 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <!--<Dialog :visible="x" @update:visible="x=$event"></Dialog>-->
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
 
 export default {
   components: {
-    Dialog
+    Dialog, Button
+  },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      console.log(1);
+      return false;
+    };
+    const f2 = () => {
+      console.log(2);
+    };
+    return {
+      x,
+      toggle,
+      f1,
+      f2
+    };
   }
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,27 +1,74 @@
 <template>
-  <div class="arw-dialog-overlay"></div>
-  <div class="arw-dialog-wrapper">
-    <div class="arw-dialog">
-      <header>标题 <span class="arw-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="arw-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="arw-dialog-wrapper">
+      <div class="arw-dialog">
+        <header>标题
+          <span @click="close" class="arw-dialog-close"></span>
+        </header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from './Button.vue';
 
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    ok: {
+      type: Function
+    },
+    cancel: {
+      type: Function
+    }
+  },
   components: {
     Button,
   },
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit('cancel');
+      close();
+    };
+
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel
+    };
+  }
 };
 </script>
 

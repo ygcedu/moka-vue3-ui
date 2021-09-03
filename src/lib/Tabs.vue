d9b7660feb33251a30c1f08f05dd8f88210672fe
ygcedu <937601471@qq.com>
2021-09-04 00:55:17 +0800

    优化1：干掉 navItems

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="arw-tabs">
     <div class="arw-tabs-nav" ref="container">
-      <div class="arw-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if(el) navItems[index]=el }"
+      <div class="arw-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if(t===selected) selectedItem=el }"
            @click="select(t)"
            :class="{selected: t=== selected}"
            :key="index">{{ t }}
@@ -27,17 +27,14 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      // const result = divs.filter(div => div.classList.contains('selected'))[0];
-      const result = divs.find(div => div.classList.contains('selected'));
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = result.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + 'px';
     };
@@ -70,7 +67,7 @@ export default {
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container
     };

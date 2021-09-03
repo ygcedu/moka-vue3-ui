6e03c014be2104ed8392350f297f9a4f722aa539
ygcedu <937601471@qq.com>
2021-09-04 00:50:45 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="arw-tabs">
-    <div class="arw-tabs-nav">
-      <div class="arw-tabs-nav-item"
-           :class="{selected: t === selected}"
-           v-for="(t,index) in titles" :key="index"
-           @click="select(t)">
-        {{ t }}
+    <div class="arw-tabs-nav" ref="container">
+      <div class="arw-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if(el) navItems[index]=el }"
+           @click="select(t)"
+           :class="{selected: t=== selected}"
+           :key="index">{{ t }}
       </div>
+      <div class="arw-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="arw-tabs-content">
-      <component class="arw-tabs-content-item" :class="{selected:c.props.title === selected}" v-for="c in defaults"
+      <component class="arw-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults"
                  :is="c"/>
+
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
-import {computed} from 'vue';
+import {computed, onMounted, onUpdated, ref} from 'vue';
 
 export default {
   props: {
@@ -26,32 +27,55 @@ export default {
     }
   },
   setup(props, context) {
-    const defaults = context.slots.default();
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      // const result = divs.filter(div => div.classList.contains('selected'))[0];
+      const result = divs.find(div => div.classList.contains('selected'));
+      const {width} = result.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {left: left1} = container.value.getBoundingClientRect();
+      const {left: left2} = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + 'px';
+    };
 
+    // 只在第一次渲染执行
+    onMounted(x);
+
+    onUpdated(x);
+
+    const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tab');
       }
     });
-
     const current = computed(() => {
       console.log('重新 return');
       return defaults.filter((tag) => {
         return tag.props.title === props.selected;
       })[0];
     });
-
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-
     const select = (title: string) => {
       context.emit('update:selected', title);
     };
-
-    return {defaults, titles, current, select};
+    return {
+      defaults,
+      titles,
+      current,
+      select,
+      navItems,
+      indicator,
+      container
+    };
   }
-};
+}
 </script>
 
 <style lang="scss">
@@ -63,6 +87,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -77,14 +102,22 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
-  }
 
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      //transition: left 2s;
+      transition: all 250ms;
+    }
+  }
   &-content {
     padding: 8px 0;
-
     &-item {
       display: none;
-
       &.selected {
         display: block;
       }

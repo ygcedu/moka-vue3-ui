c37389d3217b4b5b6db6b10ddea0fa978d6fdd2d
ygcedu <937601471@qq.com>
2021-09-04 22:39:42 +0800

    从 iconfont.cn 引入图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-arrow-down"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -22,7 +26,10 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$color: #109790;
+
 .topnav {
+  color: $color;
   //background: pink;
   display: flex;
   padding: 16px;
@@ -37,6 +44,11 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+
+    > svg {
+      width: 32px;
+      height: 32px;
+    }
   }
 
   > .menu {

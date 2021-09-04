b0a5f064b9b2d703fa9acf433228512d3d024ddb
ygcedu <937601471@qq.com>
2021-09-04 22:43:28 +0800

    使用本地的 svg.js

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './index.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
 import {router} from './router';
+// 本地引入阿里图标
+import './lib/svg';
 
 const app = createApp(App);
 app.use(router);

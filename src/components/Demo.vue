7baef4beb7991ad8031e17f1ca7559b16c2ba13d
ygcedu <937601471@qq.com>
2021-09-05 17:05:33 +0800

    显示 demo 标题

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,38 +1,37 @@
 <template>
-  <div>
-    <h1>Switch 组件示例 </h1>
-    <Demo :component="Switch1Demo"/>
-    <Demo :component="Switch2Demo"/>
+  <div class="demo">
+    <h2>{{ component.__sourceCodeTitle }}</h2>
+    <div class="demo-component">
+      <component :is="component"/>
+    </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+        <pre class="language-html"
+             v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Switch1Demo from '../components/Switch1.demo.vue';
-import Switch2Demo from '../components/Switch2.demo.vue';
 import Button from '../lib/Button.vue';
-import Demo from '../components/Demo.vue';
-import {ref} from 'vue';
 import 'prismjs';
 import 'prismjs/themes/prism.css';
 
 const Prism = (window as any).Prism;
 
-// console.log(Switch1Demo.__sourceCode);
 export default {
   components: {
-    Button,
-    Demo
+    Button
+  },
+  props: {
+    component: Object
   },
   setup() {
-    const bool = ref(false);
-    return {
-      bool,
-      Switch1Demo,
-      Switch2Demo,
-      Prism
-    };
+    return {Prism};
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>

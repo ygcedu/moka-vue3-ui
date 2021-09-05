00645ea670a94e05f00b28f63259cfad82ede575
ygcedu <937601471@qq.com>
2021-09-05 20:41:22 +0800

    解决遗留问题
    问题：build 环境动态加载 md 文件，看不了 markdown 内容
    原因：rollup 无法识别运行期需要动态加载的 md

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -4,23 +4,13 @@
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
 
 export default {
   props: {
-    path: {
+    content: {
       type: String,
       required: true
     }
-  },
-  setup(props) {
-    const content = ref<string>(null);
-    // hack: 可能会因为网络环境差异步加载缓慢，可以加一个 loading 效果
-    // 异步动态 import
-    import(props.path).then(result => {
-      content.value = result.default;
-    });
-    return {content};
   }
 };
 </script>

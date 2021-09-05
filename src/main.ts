964a81f95d4e5cd96e9366fdca9fc59005a5cce9
ygcedu <937601471@qq.com>
2021-09-05 12:47:35 +0800

    引入 github 样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './index.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
 import {router} from './router';
+import 'github-markdown-css';
 // 本地引入阿里图标
 // import './lib/svg';
 

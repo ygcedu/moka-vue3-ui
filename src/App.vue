e3f3290056bb092bb9680a663ad9af5cc7d37e8c
ygcedu <937601471@qq.com>
2021-09-06 21:05:07 +0800

    使用 Vue3 新特性 setup

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,18 @@
   <router-view/>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import {provide, ref} from 'vue';
 import {router} from './router';
 
-export default {
-  name: 'App',
-  setup() {
-    const width = document.documentElement.clientWidth;
-    const asideVisible = ref(width <= 500 ? false : true);
-    provide('asideVisible', asideVisible);// set
+const width = document.documentElement.clientWidth;
+const asideVisible = ref(width <= 500 ? false : true);
+provide('asideVisible', asideVisible);// set
 
-    // hack: 这里不管什么路由切换都会触发隐藏侧边栏菜单效果
-    router.afterEach(() => {
-      if (width <= 500) {
-        asideVisible.value = false;
-      }
-    });
+// hack: 这里不管什么路由切换都会触发隐藏侧边栏菜单效果
+router.afterEach(() => {
+  if (width <= 500) {
+    asideVisible.value = false;
   }
-};
+});
 </script>

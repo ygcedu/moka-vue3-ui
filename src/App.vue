3286a33fcdb46d8f0fdf5778d6b2162a3b2980c8
ygcedu <937601471@qq.com>
2021-09-14 20:41:55 +0800

    引入 nprogress 给 router-link 加过渡条效果

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,20 @@
 <script lang="ts">
 import {provide, ref} from 'vue';
 import {router} from './router';
+import progress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth;
     const asideVisible = ref(width <= 500 ? false : true);
+
+    router.beforeEach((to, from, next) => {
+      progress.start();
+      next();
+    });
+
     provide('asideVisible', asideVisible);// set
 
     // hack: 这里不管什么路由切换都会触发隐藏侧边栏菜单效果
@@ -18,7 +26,21 @@ export default {
       if (width <= 500) {
         asideVisible.value = false;
       }
+      progress.done();
+      window.scrollTo(0, 0);
     });
   }
 };
 </script>
+
+<style lang="scss">
+#nprogress {
+  .bar {
+    background: #6b9ab8 !important; //自定义颜色
+  }
+
+  .spinner-icon {
+    border-color: #e5f2fa transparent transparent #e5f2fa;
+  }
+}
+</style>

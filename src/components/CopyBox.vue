3b8f83d5f3921698dd3fe4c37e409a1f021aca31
ygcedu <937601471@qq.com>
2021-09-18 01:01:46 +0800

    源码复制按钮复制成功显示打勾图标

--- src/components/CopyBox.vue
+++ src/components/CopyBox.vue
@@ -1,9 +1,12 @@
 <template>
-<span class="code-copy" @click="copy" @mouseout="tip='复制代码'"
+<span class="code-copy" @click="copy" @mouseout="clear"
       data-clipboard-action="copy"
       :data-clipboard-text="sourceCode">
   <Tooltip :content="tip">
-    <svg class="icon">
+    <svg class="icon" v-if="copied">
+      <use xlink:href="#icon-success"></use>
+    </svg>
+    <svg class="icon" v-else>
       <use xlink:href="#icon-copy"></use>
     </svg>
   </Tooltip>
@@ -26,21 +29,27 @@ export default {
   setup(props) {
     const {sourceCode} = props;
     const tip = ref('复制代码');
+    const copied = ref(false);
     const copy = () => {
       const clipboard = new Clipboard('.code-copy');
       clipboard.on('success', e => {
         tip.value = '复制成功';
+        copied.value = true;
         // 释放内存
         clipboard.destroy();
       });
       clipboard.on('error', e => {
         // 不支持复制
-        tip.value = '该浏览器不支持自动复制';
+        tip.value = '该浏览器不支持自动复制！';
         // 释放内存
         clipboard.destroy();
       });
     };
-    return {copy, tip, sourceCode};
+    const clear = () => {
+      tip.value = '复制代码';
+      copied.value = false;
+    };
+    return {copy, tip, sourceCode, clear, copied};
   }
 };
 </script>
